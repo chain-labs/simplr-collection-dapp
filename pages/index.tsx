e7b114509a53fd8501fd875a29ef661d7a956aa6
angel112 <ngllakra@gmail.com>
2022-01-05 03:59:09 +0530

    added configs and husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import Box from "src/components/Box";
+import Box from 'src/components/Box';
 
 const HomePage = () => {
-  return <Box>
-    Hi
-  </Box>;
+	return (
+		<Box color="seaweed" bg="red">
+			Hi! man
+			<Box color="yellow" bg="green">
+				This is me
+			</Box>
+		</Box>
+	);
 };
 
 export default HomePage;
