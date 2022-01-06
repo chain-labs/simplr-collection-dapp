41b2a9f4e401ba53861b30855dca0a82a676ca34
angel112 <angel.lakra@chainlabs.in>
2022-01-06 15:01:20 +0530

    Verified store function with state updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Box from 'src/components/Box';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount, selectCount } from 'src/redux/reducers/counterSlice';
 
 const HomePage = () => {
+	const dispatch = useDispatch();
+	const count = useSelector(selectCount);
 	return (
 		<Box color="seaweed" bg="red">
 			Hi! man
 			<Box color="yellow" bg="green">
-				This is the home page
+				This is the home page with {count}
+			</Box>
+			<Box as="button" onClick={() => dispatch(increment())}>
+				+
+			</Box>
+			<Box as="button" onClick={() => dispatch(decrement())}>
+				-
 			</Box>
 		</Box>
 	);
