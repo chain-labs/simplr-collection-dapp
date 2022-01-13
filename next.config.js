507277825c7940d381dc31b050f1d797259d7ca6
angel112 <angel.lakra@chainlabs.in>
2022-01-13 12:56:47 +0530

    input with label and helper

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-	reactStrictMode: true,
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require('next-transpile-modules')(['gsap']);
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+	images: {
+		domains: ['nftfy.mypinata.cloud'],
+	},
 };
+module.exports = withPlugins([
+	[
+		withTM({
+			webpack(config) {
+				config.module.rules.push({
+					test: /\.(jpg|gif|svg|eot|ttf|woff|woff2)$/,
+					use: ['@svgr/webpack'],
+				});
+				return config;
+			},
+		}),
+	],
+	nextConfig,
+]);
---
+++ src/components/LabelledTextInput.tsx
@@ -0,0 +1,31 @@
+import Box from './Box';
+import Text from './Text';
+import TextInput from './TextInput';
+
+interface Props {
+	label: string;
+	helperText?: string;
+	placeholder?: string;
+	regexp?: string;
+	disabled?: boolean;
+	value: any;
+	setValue: (any) => void;
+}
+
+const LabelledTextInput = ({ label, helperText, placeholder, regexp, disabled, value, setValue }: Props) => {
+	return (
+		<Box overflow="visible">
+			<Text as="h6" mb="mxs" color="simply-gray">
+				{label}
+			</Text>
+			<TextInput {...{ disabled, regexp, placeholder, value, setValue }} />
+			{!!helperText && (
+				<Text as="h6" mt="mxs" color="simply-gray">
+					{helperText}
+				</Text>
+			)}
+		</Box>
+	);
+};
+
+export default LabelledTextInput;
