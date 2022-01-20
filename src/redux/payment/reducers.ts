f9b62110ccc5a2ccca44e091b5d1799519c37b9e
angel112 <angel.lakra@chainlabs.in>
2022-01-21 03:50:34 +0530

    payment state store added

---
+++ src/redux/payment/reducers.ts
@@ -0,0 +1,38 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { addBeneficiary, removeBeneficiary } from '.';
+import { setPaymentDetails } from './actions';
+import { PaymentState } from './types';
+
+const intitalState: PaymentState = {
+	royalties: {
+		account: '',
+		value: null,
+	},
+	paymentSplitter: {
+		payees: [],
+		shares: [],
+	},
+};
+
+export const paymentReducer = createReducer(intitalState, (builder) => {
+	builder
+		.addCase(setPaymentDetails, (state, action) => {
+			const updateState: PaymentState = action.payload;
+			const newState = { ...state, ...updateState };
+			state = newState;
+			return state;
+		})
+		.addCase(addBeneficiary, (state, action) => {
+			state.paymentSplitter.payees.push(action.payload.payee);
+			state.paymentSplitter.shares.push(action.payload.shares);
+			return state;
+		})
+		.addCase(removeBeneficiary, (state, action) => {
+			const index = state.paymentSplitter.payees.indexOf(action.payload);
+			if (index !== -1) {
+				state.paymentSplitter.payees.splice(index, 1);
+				state.paymentSplitter.shares.splice(index, 1);
+			}
+			return state;
+		});
+});
