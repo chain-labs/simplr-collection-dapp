9da5e49a329192a067d8e796da7ce7f0a4eb3e56
angel112 <angel.lakra@chainlabs.in>
2022-01-21 18:04:57 +0530

    created route for create form and added redirect to this route with minimal layout also added

---
+++ pages/create/index.tsx
@@ -0,0 +1,7 @@
+import CreateComp from 'containers/create';
+
+const CreatePage = () => {
+	return <CreateComp />;
+};
+
+export default CreatePage;
---
+++ src/containers/create/index.tsx
@@ -0,0 +1,14 @@
+import Box from 'src/components/Box';
+import Text from 'src/components/Text';
+
+const CreateComp = () => {
+	return (
+		<Box pt="mxxxl" mx="auto" width="63rem" minHeight="100vh" overflowX="visible">
+			<Text as="headline" mx="auto">
+				Create new collection
+			</Text>
+		</Box>
+	);
+};
+
+export default CreateComp;
