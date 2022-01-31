13e98bd1fb150c74c5720a709d6fe5d84eedc15d
angel112 <angel.lakra@chainlabs.in>
2022-01-31 19:31:16 +0530

    added dynamic routes and new state to read data from but most probably will reverse this

---
+++ pages/admin/[id].tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import AdminDashboardComponent from 'containers/admin';
+import { ethers } from 'ethers';
+import { GetServerSideProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getContractDetails } from 'src/ethereum/useCustomContract';
+import useEthers from 'src/ethereum/useEthers';
+
+const AdminDashboardPage = () => {
+	const router = useRouter();
+	const { id } = router.query;
+	const [provider] = useEthers();
+	const [metadata, setMetadata] = useState();
+	const getMetadata = async () => {
+		const abi = getContractDetails('AffiliateCollection');
+		const contract = new ethers.Contract(`${id}`, abi, provider);
+		const qid = await contract.callStatic.metadata();
+		const res = await axios.get(`https://simplr.mypinata.cloud/ipfs/${qid}`);
+		setMetadata(res.data);
+	};
+
+	useEffect(() => {
+		if (id) {
+			getMetadata();
+		}
+	}, [id]);
+
+	return <AdminDashboardComponent metadata={metadata} id={id} />;
+};
+
+export default AdminDashboardPage;
--- src/containers/admin/index.tsx
+++ src/containers/admin/index.tsx
@@ -1,31 +1,65 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import Box from 'src/components/Box';
 import Text from 'src/components/Text';
+import useContract from 'src/ethereum/useContract';
+import useCustomContract, { getContractDetails } from 'src/ethereum/useCustomContract';
+import useEthers from 'src/ethereum/useEthers';
 import theme from 'src/styleguide/theme';
 import CollectionPage from './components/CollectionPage';
 import PaymentsPage from './components/PaymentsPage';
 
-const AdminDashboardComponent = () => {
+const AdminDashboardComponent = ({ metadata, id }) => {
 	const [step, setStep] = useState(0);
+	const [provider] = useEthers();
+	const [contract, setContract] = useState<ethers.Contract>();
+
+	useEffect(() => {
+		if (id) {
+			const abi = getContractDetails('AffiliateCollection');
+			const contract = new ethers.Contract(id, abi, provider);
+			setContract(contract);
+		}
+	}, [id]);
+
+	useEffect(() => {
+		console.log({ metadata });
+	}, [metadata]);
 
 	const getPage = (step) => {
 		if (step === 0) {
-			return <CollectionPage />;
+			return <CollectionPage contract={contract} />;
 		}
 		if (step === 1) {
-			return <PaymentsPage />;
+			return <PaymentsPage contract={contract} />;
 		}
 	};
 
 	return (
 		<Box>
 			<Toaster position="top-center" />
-			<Box bg="yellow-40" height="36rem" width="100%" />
+			<Box
+				backgroundImage={`url(${metadata?.collectionDetails.bannerImageUrl})`}
+				backgroundPosition="top"
+				backgroundColor="black"
+				backgroundRepeat="no-repeat"
+				height="36rem"
+				width="100%"
+			/>
 			<Box mt={`-${theme.space.wxs}`} mb="wxs" mx="auto" column center>
-				<Box bg="simply-blue" height="10rem" width="10rem" borderRadius="50%" mb="wxxs" />
-				<Text as="h3">The Boomer Gang Collective</Text>
-				<Text as="h3">(TGBC)</Text>
+				<Box
+					backgroundImage={`url(${metadata?.collectionDetails.logoUrl})`}
+					backgroundPosition="center"
+					backgroundSize="contain"
+					height="10rem"
+					width="10rem"
+					borderRadius="50%"
+					mb="wxxs"
+				/>
+				<Text as="h3">{metadata?.collectionDetails.name}</Text>
+				<Text as="h3">({metadata?.collectionDetails.symbol})</Text>
 			</Box>
 			<Box row between mx="auto" width="21.6rem">
 				<Text
