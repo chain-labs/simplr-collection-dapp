aa2033f18dbb3c00890eace210853fe1a2312906
angel112 <angel.lakra@chainlabs.in>
2022-02-01 15:20:14 +0530

    removed redux and implemented useState

--- src/redux/dashboard/selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from '../store';
-
-export const selectDashboardInfo = (state: RootState) => state.dashboard.collection;
-
-export const dashboardSelector = createSelector(selectDashboardInfo, (state) => state);
