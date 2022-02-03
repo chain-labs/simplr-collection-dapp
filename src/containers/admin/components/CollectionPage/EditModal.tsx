fe82e2bf9e842831526010e3b93a23742024ff62
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-02-03 05:35:27 +0530

    edit modal added

---
+++ src/containers/admin/components/CollectionPage/EditModal.tsx
@@ -0,0 +1,155 @@
+import { useEffect, useState } from 'react';
+import Box from 'src/components/Box';
+import ButtonComp from 'src/components/Button';
+import If from 'src/components/If';
+import Modal from 'src/components/Modal';
+import Text from 'src/components/Text';
+import TextInput from 'src/components/TextInput';
+import { editSelector } from 'src/redux/edit';
+import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
+import { presaleWhitelistSelector, removeWhitelist } from 'src/redux/sales';
+
+interface props {
+	visible: boolean;
+	setVisible: (boolean) => void;
+	edit?: string;
+	data?: string;
+	label?: any;
+	placeholder?: any;
+}
+
+const EditModal = ({ visible, setVisible, edit, data, label }: props) => {
+	const modalData = useAppSelector(editSelector);
+	const [value, setValue] = useState('');
+	const [step, setStep] = useState(0);
+
+	const handleAction = () => {
+		if (step === 0) {
+			setStep(1);
+		}
+		if (step === 1) {
+			setStep(2);
+		}
+		if (step === 2) {
+			setVisible(false);
+		}
+	};
+
+	if (visible) {
+		return (
+			<Modal visible={visible}>
+				<Box
+					mx="auto"
+					bg="simply-white"
+					// height="35rem"
+					width="49rem"
+					borderRadius="16px"
+					p="mxxxl"
+					position="absolute"
+					top="50%"
+					left="50%"
+					transform="translate(-50%, -50%)"
+					column
+				>
+					<If
+						condition={step === 0}
+						then={
+							<Box>
+								<Text as="b3" fontWeight="medium" mb="ms">
+									{modalData.label}
+								</Text>
+								<TextInput
+									type={modalData.type}
+									placeholder={modalData.data}
+									value={value}
+									width="100%"
+									setValue={setValue}
+									disableValidation
+								/>
+								<Box mt="mxxl" />
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One">
+									OLD DATA : {modalData.data}
+								</Text>
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One" display="flex">
+									ESTIMATED GAS COST :{' '}
+									<Text as="c1" color="simply-blue">
+										0.0001 ETH or 1 USD.
+									</Text>
+								</Text>
+							</Box>
+						}
+					/>
+					<If
+						condition={step === 1}
+						then={
+							<Box>
+								<Text as="h4" mb="ms" fontFamily="Switzer">
+									Confirm Change
+								</Text>
+								<Text as="h6" mt="mxxl" color="#52575C" fontFamily="Switzer">
+									Every change in the smart contract costs gas. Do you want to commit these changes?
+								</Text>
+
+								<Box mt="ml" />
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One" display="flex">
+									GAS COST :{' '}
+									<Text as="c1" color="simply-blue">
+										0.0001 ETH or 1 USD.
+									</Text>
+								</Text>
+							</Box>
+						}
+					/>
+					<If
+						condition={step === 2}
+						then={
+							<Box>
+								<Text as="h4" mb="ms" fontFamily="Switzer">
+									{modalData.label}
+								</Text>
+								<Text as="h6" mt="mxxl" color="#52575C" fontFamily="Switzer">
+									Successfully changed the {modalData.label}. Changes have been reflected on your dashboard.
+								</Text>
+
+								<Box mt="mm" />
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One" display="flex">
+									OLD {modalData.label.toUpperCase()} :{' '}
+									<Text as="c1" color="simply-blue">
+										{modalData.data.slice(0, 4)}...{modalData.data.slice(38, 42)}
+									</Text>
+								</Text>
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One" display="flex" mt="mxxs">
+									NEW {modalData.label.toUpperCase()} :{' '}
+									<Text as="c1" color="simply-blue">
+										{modalData.data.slice(0, 4)}...{modalData.data.slice(38, 42)}
+									</Text>
+								</Text>
+
+								<Text as="c1" color="gray-00" fontFamily="Open Sauce One" display="flex" mt="mm">
+									GAS COST :{' '}
+									<Text as="c1" color="simply-blue">
+										0.0001 ETH or 1 USD.
+									</Text>
+								</Text>
+							</Box>
+						}
+					/>
+
+					<ButtonComp bg="primary" height="40px" onClick={handleAction} mt="mxl">
+						<Text as="h6" fontFamily="Switzer">
+							{step === 0 ? 'proceed' : step === 1 ? 'Commit Change' : 'Return to Dashboard'}
+						</Text>
+					</ButtonComp>
+					<ButtonComp bg="secondary" height="40px" onClick={() => setVisible(false)} mt="ml">
+						<Text as="h6" fontFamily="Switzer">
+							Cancel
+						</Text>
+					</ButtonComp>
+				</Box>
+			</Modal>
+		);
+	}
+	return <></>;
+};
+
+export default EditModal;
--- src/containers/admin/components/CollectionPage/index.tsx
+++ src/containers/admin/components/CollectionPage/index.tsx
@@ -1,14 +1,19 @@
 import { ethers } from 'ethers';
-import { CurrencyEth, DotsThreeOutlineVertical, ImageSquare, Timer, User } from 'phosphor-react';
+import { CurrencyEth, DotsThreeOutlineVertical, ImageSquare, Timer, User, X } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import Box from 'src/components/Box';
 import ButtonComp from 'src/components/Button';
 import If from 'src/components/If';
 import LabelledTextInput from 'src/components/LabelledTextInput';
+import Modal from 'src/components/Modal';
 import Text from 'src/components/Text';
 import TextInput from 'src/components/TextInput';
+import WhitelistModal from 'src/containers/create/components/SalesPage/WhitelistModal';
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import theme from 'src/styleguide/theme';
+import DashboardCard from './DashboardCard';
+import EditModal from './EditModal';
+// import EditModal from './EditModal';
 
 const CollectionPage = ({ contract }) => {
 	const dispatch = useAppDispatch();
@@ -16,7 +21,9 @@ const CollectionPage = ({ contract }) => {
 	const [isEditableCollectionUri, setIsEditableCollectionUri] = useState(false);
 	const [airdropAddress, setAirdropAddress] = useState('');
 	const [edited, setEdited] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 	const [adminAddress, setAdminAddress] = useState('0xd18Cd50a6bDa288d331e3956BAC496AAbCa4960d');
+	const [edit, setEdit] = useState('');
 	const [collection, setCollection] = useState({
 		maxTokens: '',
 		adminAddress: '',
@@ -117,179 +124,29 @@ const CollectionPage = ({ contract }) => {
 							Edit
 						</Text>
 					</Box>
-					<TextInput
-						placeholder="https://gdrive.com/***"
-						value={collectionUri}
-						setValue={setCollectionURI}
+				</Box>
+				<Text as="h3" color="simply-blue" mt="wxl">
+					Airdrop:
+				</Text>
+				<Box mt="mxl" width="55.2rem">
+					<LabelledTextInput
+						value={airdropAddress}
+						setValue={setAirdropAddress}
+						placeholder="Enter a valid wallet address"
+						label="Airdrop NFTs:"
+						helperText="Airdrop an NFT from your reserve to any wallet address."
 						disableValidation
-						disabled={!isEditableCollectionUri}
 						width="100%"
 					/>
-					<Text as="b1" mt="mxs" color="gray-00">
-						Collection URI is the URL where your NFT media and metadata are stored.{' '}
-					</Text>
+					<Box row justifyContent="flex-end" mt="mxl" mb="wm">
+						<ButtonComp bg="tertiary" height="40px" width="9.2rem" disable={!airdropAddress}>
+							<Text as="h6">Airdrop</Text>
+						</ButtonComp>
+					</Box>
 				</Box>
-				<Box ml="wm" />
-				<Box flex={1}>
-					<Text as="h6" mb="mxs">
-						Loading Image URI
-					</Text>
-					<TextInput
-						placeholder="https://gdrive.com/somethingurl"
-						value="https://gdrive.com/somethingurl"
-						disableValidation
-						disabled
-						width="100%"
-					/>
-					<Text as="b1" mt="mxs" color="gray-00">
-						Placeholder image that will be displayed until the set reveal time.
-					</Text>
-				</Box>
-			</Box>
-			<Text as="h3" color="simply-blue" mt="wxl">
-				Airdrop:
-			</Text>
-			<Box mt="mxl" width="55.2rem">
-				<LabelledTextInput
-					value={airdropAddress}
-					setValue={setAirdropAddress}
-					placeholder="Enter a valid wallet address"
-					label="Airdrop NFTs:"
-					helperText="Airdrop an NFT from your reserve to any wallet address."
-					disableValidation
-					width="100%"
-				/>
-				<Box row justifyContent="flex-end" mt="mxl" mb="wm">
-					<ButtonComp bg="tertiary" height="40px" width="9.2rem" disable={!airdropAddress}>
-						<Text as="h6">Airdrop</Text>
-					</ButtonComp>
-				</Box>
-			</Box>
-			<Box center mb="14rem">
-				<ButtonComp bg={edited ? 'primary' : 'tertiary'} width="64rem" height="56px" mx="auto">
-					<Text as="h4">Save Changes</Text>
-				</ButtonComp>
 			</Box>
 		</Box>
 	);
 };
 
 export default CollectionPage;
-
-interface DashboardCardProps {
-	Icon: React.ReactNode;
-	text: string;
-	data?: string;
-	editable?: 'address' | 'time' | 'number' | 'status';
-	status?: 'Live' | 'Paused' | 'Sold Out' | 'Ended';
-	setData?: (any) => void;
-}
-
-const DashboardCard = ({ text, data, setData, editable, status, Icon }: DashboardCardProps) => {
-	const [drawerOpen, setDrawerOpen] = useState(false);
-	const [editing, setEditing] = useState(false);
-	const [value, setValue] = useState(data);
-
-	const handleAction = () => {
-		if (editable === 'address' || editable === 'number') {
-			setDrawerOpen(false);
-			setEditing(true);
-		}
-	};
-
-	useEffect(() => {
-		setValue(data);
-	}, [data]);
-
-	const getData = (data) => {
-		if (editable === 'address') {
-			if (data.length > 10) {
-				return `${value.substr(0, 4)}...${value.substr(-4)}`;
-			}
-			return data;
-		}
-	};
-
-	return (
-		<Box
-			row
-			width="55.2rem"
-			height="7.2rem"
-			alignItems="center"
-			border="1px solid"
-			borderColor="#DCDCE880"
-			borderRadius="8px"
-			px="mxl"
-			mb="mm"
-		>
-			<Box center width="4.8rem" height="4.8rem" borderRadius="50%" bg="blue-00">
-				{/* @ts-expect-error Icon as props */}
-				<Icon color={theme.colors['simply-blue']} size="20" weight="bold" />
-			</Box>
-			<Box row between ml="mm" width="40rem">
-				<Text as="h5">{text}</Text>
-				<If
-					condition={!!status}
-					then={
-						<Text as="h4" color={status === 'Live' || status === 'Sold Out' ? 'green-60' : 'red-50'}>
-							{status}
-						</Text>
-					}
-					else={
-						<Box
-							bg={editable === 'address' ? (editing ? 'simply-white' : 'blue-00') : 'transparent'}
-							borderRadius="8px"
-							border={editing ? '1px solid' : 'none'}
-							borderColor={editing ? '#dcdce8' : 'transparent'}
-							outline="none"
-							px={editable === 'address' || editable === 'number' ? (editing ? 'mxs' : 'mxl') : '0'}
-							py={editable === 'address' || editable === 'number' ? 'mxs' : '0'}
-							as={editing ? 'input' : null}
-							type={editing && editable === 'number' ? 'number' : 'text'}
-							value={value}
-							onChange={(e) => setValue(e.target.value)}
-							fontFamily="inherit"
-							fontSize="1.4rem"
-						>
-							{!editing ? (
-								<Text as="h4" color="simply-blue">
-									{editable === 'address' ? getData(value) : data}
-								</Text>
-							) : null}
-						</Box>
-					}
-				/>
-			</Box>
-			<If
-				condition={!!editable && (status === 'Sold Out' || status !== 'Ended')}
-				then={
-					<Box position="relative" onMouseLeave={() => setTimeout(() => setDrawerOpen(false), 1000)} cursor="pointer">
-						<Box ml="mxl" center onClick={() => setDrawerOpen(true)}>
-							<DotsThreeOutlineVertical color={theme.colors['gray-00']} size="20" weight="fill" />
-						</Box>
-						<If
-							condition={drawerOpen}
-							then={
-								<Box
-									px="mm"
-									py="mxs"
-									bg="white-00"
-									border="1px solid"
-									borderColor="white-20"
-									boxShadow="0px 2px 4px -2px rgba(24, 39, 75, 0.12), 0px 4px 4px -2px rgba(24, 39, 75, 0.08)"
-									borderRadius="4px"
-									position="absolute"
-									left="25px"
-									cursor="pointer"
-									onClick={handleAction}
-								>
-									<Text as="c3">{status === 'Live' ? 'Pause' : status === 'Paused' ? 'Resume' : 'Edit'}</Text>
-								</Box>
-							}
-						/>
-					</Box>
-				}
-			/>
-		</Box>
-	);
-};
