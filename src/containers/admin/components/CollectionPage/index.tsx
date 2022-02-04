43b37d58d880a1d0fcbb7f10275bd98379b7ce9c
angel112 <angel.lakra@chainlabs.in>
2022-02-04 20:13:28 +0530

    updated ended and started status

--- src/containers/admin/components/CollectionPage/index.tsx
+++ src/containers/admin/components/CollectionPage/index.tsx
@@ -135,7 +135,7 @@ const CollectionPage = ({ contract, metadata }) => {
 								<DashboardCard
 									Icon={Timer}
 									text="Pre-Sale"
-									status={collection.saleStartTime > Date.now() ? 'Ended' : 'Live'}
+									status={collection.saleStartTime > Date.now() / 1000 ? 'Live' : 'Ended'}
 									editable="status"
 								/>
 							}
