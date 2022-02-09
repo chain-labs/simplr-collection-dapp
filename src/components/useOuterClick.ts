2802a4fb3a9c37bdc803fb3c97937ad483d6eb74
angel112 <angel.lakra@chainlabs.in>
2022-02-10 01:29:07 +0530

    qa fix: Dropdown close on clicking outside

---
+++ src/components/useOuterClick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+export default function useOuterClick(callback) {
+	const callbackRef = useRef(); // initialize mutable ref, which stores callback
+	const innerRef = useRef(); // returned to client, who marks "border" element
+
+	// update cb on each render, so second useEffect has access to current value
+	useEffect(() => {
+		callbackRef.current = callback;
+	});
+
+	useEffect(() => {
+		document.addEventListener('click', handleClick);
+		return () => document.removeEventListener('click', handleClick);
+		function handleClick(e) {
+			// @ts-expect-error ts-migrate(2339) FIXME: Property 'current' does not exist on type 'RefObject<HTMLElement>'.
+			if (innerRef.current && callbackRef.current && !innerRef?.current?.contains(e.target)) callbackRef?.current(e);
+		}
+	}, []); // no dependencies -> stable click listener
+
+	return innerRef; // convenience for client (doesn't need to init ref himself)
+}
