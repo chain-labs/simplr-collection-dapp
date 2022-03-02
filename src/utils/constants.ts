cfab7a540cb05b58b78b8e9327e950360925a232
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-03-02 17:34:17 +0530

    fixed undefined error

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const SEAT_TOGGLE = process.env.NEXT_PUBLIC_SEAT_TOGGLE;
 
 export const toBoolean = (value: string): boolean => {
-	if (value.toLowerCase() === 'true') {
+	if (value?.toLowerCase() === 'true') {
 		return true;
 	} else {
 		return false;
