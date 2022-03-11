2ac8f7da989fed78daaa83239118f46c15e7de92
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2022-03-12 01:55:15 +0530

    banner removed

--- src/containers/create/components/Tnc.tsx
+++ src/containers/create/components/Tnc.tsx
@@ -12,6 +12,8 @@ import toast, { Toaster } from 'react-hot-toast';
 import Checkbox from 'src/components/Checkbox';
 import Markdown from 'markdown-to-jsx';
 import { PINATA_HASH } from './utils';
+import { MDXProvider } from '@mdx-js/react';
+import Navbar from 'src/components/Navbar';
 
 const Tnc = ({ setStep }) => {
 	const [provider] = useEthers();
@@ -52,46 +54,57 @@ const Tnc = ({ setStep }) => {
 	};
 
 	return (
-		<Box center>
-			<Toaster
-				position="top-center"
-				toastOptions={{
-					duration: 5000,
-				}}
-			/>
-			<Box width="45.9rem">
-				<Text as="h2" pb="mm" textAlign="center" color="simply-blue">
-					Welcome to Simplr!
-				</Text>
-				<Text as="b2" mb="mm" textAlign="center" color="#52575C">
-					We know you’re excited about getting started with your first ever project on simplr and we are too. For that
-					we need to make sure that you go through our Terms & Conditions for using Simplr.
-				</Text>
-				<Box mb="30px" center>
-					<Box height="33.6rem" width="40.6rem" overflowY="scroll" overflowX="hidden" className="tnc-hidden-scrollbar">
-						<Markdown children={text} />
-					</Box>
-				</Box>
-				<Box center mb="mm">
-					<Checkbox value={checkbox} setValue={setCheckbox} disabled={loading === true} />
-					<Text as="h6" ml="mm">
-						I hereby agree to all Simplr Terms and Conditions
-					</Text>
-				</Box>
-				<Box px="mxxl">
-					<ButtonComp
-						bg="primary"
-						height="40px"
-						width="100%"
-						onClick={handleSignature}
-						disable={checkbox === false || loading === true}
-						center
-					>
-						<Box center ml="mxs" className="spin" display={loading === true ? 'flex' : 'none'}>
-							<CircleNotch size="24" />
+		<Box center minHeight="100vh" right="0" position="absolute" top="0" zIndex="16" backgroundColor="white">
+			<Navbar />
+			<Box width="100vw" minHeight="100vh" pt="16rem" bg="simply-white" column center overflowY="auto" mb="0">
+				<Box top="12px">
+					<Toaster
+						position="top-center"
+						toastOptions={{
+							duration: 5000,
+						}}
+					/>
+					<Box width="45.9rem" top="80px">
+						<Text as="h2" pb="mm" textAlign="center" color="simply-blue">
+							Welcome to Simplr!
+						</Text>
+						<Text as="b2" mb="mm" textAlign="center" color="#52575C">
+							We know you’re excited about getting started with your first ever project on simplr and we are too. For
+							that we need to make sure that you go through our Terms & Conditions for using Simplr.
+						</Text>
+						<Box mb="30px" center>
+							<Box
+								height="33.6rem"
+								width="40.6rem"
+								overflowY="scroll"
+								overflowX="hidden"
+								className="tnc-hidden-scrollbar"
+							>
+								<Markdown>{text}</Markdown>
+							</Box>
 						</Box>
-						<Text as="h5">{loading ? 'Opening Metasmask' : 'Sign and Proceed'}</Text>
-					</ButtonComp>
+						<Box center mb="mm">
+							<Checkbox value={checkbox} setValue={setCheckbox} disabled={loading === true} />
+							<Text as="h6" ml="mm">
+								I hereby agree to all Simplr Terms and Conditions
+							</Text>
+						</Box>
+						<Box px="mxxl">
+							<ButtonComp
+								bg="primary"
+								height="40px"
+								width="100%"
+								onClick={handleSignature}
+								disable={checkbox === false || loading === true}
+								center
+							>
+								<Box center ml="mxs" className="spin" display={loading === true ? 'flex' : 'none'}>
+									<CircleNotch size="24" />
+								</Box>
+								<Text as="h5">{loading ? 'Opening Metasmask' : 'Sign and Proceed'}</Text>
+							</ButtonComp>
+						</Box>
+					</Box>
 				</Box>
 			</Box>
 		</Box>
