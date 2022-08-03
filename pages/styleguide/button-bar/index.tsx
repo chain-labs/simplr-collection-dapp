6349079305d22a458186aaa5e84f80e388a5e334
angel112 <angel.lakra@chainlabs.in>
2022-08-03 21:46:01 +0530

    removed unused routes

--- pages/styleguide/button-bar/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ButtonBar from 'src/containers/styleguide/button-bar';
-
-const StyleguideButtonBarComponentsPage = () => {
-	return <ButtonBar />;
-};
-
-export default StyleguideButtonBarComponentsPage;
--- pages/styleguide/inputs/index.tsx
+++
@@ -1,7 +0,0 @@
-import InputComponents from 'src/containers/styleguide/inputs';
-
-const InputPage = () => {
-	return <InputComponents />;
-};
-
-export default InputPage;
